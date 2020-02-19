b98fb5f32ad766cedc065f10a0524bc0247c18b5
kly001 <karenly1961@gmail.com>
2020-02-18 22:28:19 -0500

    Still trying to deploy backend

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`\n*** Server up on PORT ${PORT} ***\n`));
+const PORT = process.env.PORT ||5000;
+server.listen(PORT, () => console.log(`\n*** Server running on PORT ${PORT} ***\n`));
