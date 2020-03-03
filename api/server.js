bda1c3070fbf1fb7dd0eaf4ded90e72ec9e6c385
Carl-Stanley <cds.beml@gmail.com>
2020-03-02 19:40:25 -0700

    Commit

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-//const authenticate = require('../auth/authenticate-middleware.js');
+const authenticate = require('../auth/authenticate-middleware.js');
 
 const authRouter = require('../auth/auth-router.js');
 
--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,4 +3,6 @@ const knexConfig = require('../knexfile')
 
 const env = process.env.DATABASE_ENV || "development";
 
-module.exports = knex(knexConfig[environment]);
+//module.exports = knex(knexConfig[environment]);
+
+module.exports = knex(knexConfig.development);
