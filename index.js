c0a42174b4ce869f89d5ed947f66fa8c010a44c5
sagdish <forsi1@yahoo.com>
2020-03-05 15:53:46 -0800

    initial commit, before restructure

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`\n*** Server running on PORT ${PORT} ***\n`));
+
+
+// this will be entry point for GraphQL server
