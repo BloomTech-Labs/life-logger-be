73e9975b7929be70fc82f11d2f270cd6880b63cd
sagdish <forsi1@yahoo.com>
2020-03-06 13:58:45 -0800

    remove REST api related files and folders

--- data/dbConfig.js
+++
@@ -1,8 +0,0 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile')
-
-const env = process.env.DATABASE_ENV || "development";
-
-//module.exports = knex(knexConfig[environment]);
-
-module.exports = knex(knexConfig.development);
