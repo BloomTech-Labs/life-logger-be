0d2b449021d59f155a04a6402369895f01005402
sagdish <forsi1@yahoo.com>
2020-03-06 20:30:58 -0800

    basic setup, server respons with dummy data

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-// const resolvers = {
-//   Query,
-//   Mutation,
-//   Subscription,
-//   User,
-//   Link,
-//   Vote,
-// }
+const Query = require('./src/resolvers/Query')
+const Mutation = require('./src/resolvers/Mutation')
+const User = require('./src/resolvers/User')
+const Event = require('./src/resolvers/Event')
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Event,
+}
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma }
+  // context: { prisma }
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
---
+++ src/resolvers/Query.js
@@ -0,0 +1,17 @@
+// test user before implementing database
+const testUsers = [{
+  id: 'id-0',
+  username: 'John Doe',
+  password: 'pass',
+  email: 'john@mail.com'
+}]
+
+
+function allUsers() {
+  return testUsers
+}
+
+module.exports = {
+  allUsers,
+}
+
