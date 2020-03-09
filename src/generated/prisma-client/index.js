72321547b960a3b7223d757a39d5b0b9676c17aa
sagdish <forsi1@yahoo.com>
2020-03-09 16:46:14 -0700

    prisma datamodel configure, WIP

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Event",
+    embedded: false
+  },
+  {
+    name: "Location",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/life-logger/life-logger-be/dev`
+});
+exports.prisma = new exports.Prisma();
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -8,10 +8,30 @@ const testUsers = [{
 
 
 function allUsers() {
-  return testUsers
+  return context.prisma.users();
+  // return testUsers
 }
 
 module.exports = {
   allUsers,
 }
 
+
+
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: (root, args, context, info) => {
+      return context.prisma.links()
+    },
+  },
+  Mutation: {
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description,
+      })
+    },
+  },
+}
