ef23b516c9683e63ed61f7b4b9c518ab98a6deb6
Carl-Stanley <cds.beml@gmail.com>
2020-03-10 18:57:07 -0700

    Commit - added addtional tests.

--- testspec.js
+++ testspec.js
@@ -5,7 +5,7 @@ const request = require("supertest");
 const db = require("./data/dbConfig");
 
 
-
+// login end point test
 describe('POST', ()=> {
     describe('GET', ()=>{
         it('return a 404 OK', ()=>{
@@ -25,6 +25,7 @@ describe('POST', ()=> {
     })
 })
 
+// register end point test
 describe('POST', ()=> {
     describe('GET', ()=>{
         it('return a 404 OK', ()=>{
@@ -37,6 +38,46 @@ describe('POST', ()=> {
         it('JSON response', ()=>{
             return request(server)
             .get('/api/auth/register')
+            .then(res => {
+                expect(res.type).toMatch('text/html')
+            })  
+         })  
+    })
+})
+
+// events end point test
+describe('GET', ()=> {
+    describe('GET', ()=>{
+        it('return a 404 OK', ()=>{
+            return request(server)
+            .get('/api/events')
+            .then(res => {
+                expect(res.status).toBe(404)
+            })
+        })
+        it('JSON response', ()=>{
+            return request(server)
+            .get('/api/events')
+            .then(res => {
+                expect(res.type).toMatch('text/html')
+            })  
+         })  
+    })
+})
+
+// events end point test one event
+describe('GET', ()=> {
+    describe('GET', ()=>{
+        it('return a 404 OK', ()=>{
+            return request(server)
+            .get('/api/events/1')
+            .then(res => {
+                expect(res.status).toBe(404)
+            })
+        })
+        it('JSON response', ()=>{
+            return request(server)
+            .get('/api/events/1')
             .then(res => {
                 expect(res.type).toMatch('text/html')
             })  
