19b7be3ea1f971c2238682db96ac35cde11c2050
kly001 <karenly1961@gmail.com>
2020-03-10 21:04:08 -0400

    Installed cross-env & supertest

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: 'test1', password: 'test', email: 'test1@test.net' },
-        {id: 2, username: 'test2', password: 'test', email: 'test2@test.net' },
-        {id: 3, username: 'test3', password: 'test', email: 'test3@test.net' }
+        { username: 'test1', password: 'test', email: 'test1@test.net' },
+        { username: 'test2', password: 'test', email: 'test2@test.net' },
+        { username: 'test3', password: 'test', email: 'test3@test.net' }
       ]);
     });
 };
