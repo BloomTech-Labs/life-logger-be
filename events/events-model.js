ce7bed5468ee90fa16ff910ae82ce8b54ce716db
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-11 22:41:05 -0400

    updates event apis

--- events/events-model.js
+++ events/events-model.js
@@ -1,52 +1,45 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
-    find,
-    findBy,
-    findById,
-	findByUserId,
-    add,
-    update,
-    remove
-}
+  find,
+  findBy,
+  findById,
+  findByUserId,
+  add,
+  update,
+  remove
+};
 
 // Get all events
 function find() {
-    return db('events');
+  return db('events');
 }
 
-
 function findBy(filter) {
-    return db('events').where(filter)
+  return db('events').where(filter);
 }
 
 // Get events by user ID
 function findByUserId(user_id) {
-	return db('events').where({user_id});
+  return db('events').where({ user_id });
 }
- 
-// Get events by event ID 
-function findById(event_id) {
-    return db('events').where({event_id}).first();
-    }
+
+// Get events by event ID
+function findById(id) {
+  return db('events').where({ id }).first();
+}
 
 // Add a new event
 function add(event) {
-        return db('events').insert(event)
-        .then(ids => {
-        return findById(ids[0])
- })
+  return db('events').insert(event).returning('id');
 }
 
-    
 // Update existing event
-function update(event_id, changes) {
-    return db('events')
-     .where({ event_id })
-     .update(changes, '*');
+function update(id, changes) {
+  return db('events').where({ id }).update(changes, '*');
 }
-      
+
 // Delete an event
-function remove(event_id) {
-    return db('events').where({event_id}).del()
+function remove(id) {
+  return db('events').where({ id }).del();
 }
--- events/events-router.js
+++ events/events-router.js
@@ -1,111 +1,129 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
 const Events = require('./events-model.js');
 
 const nopass = require('../auth/authenticate-middleware');
 
-// Return All events 
-router.get('/', nopass , (req, res) => {
-    Events.find()
-.then(event => {
-    res.json(event);
-})
-.catch(err => {
- console.log(err);
- res.status(500).json({ message: "Error Fetching events!"});
-
-})
-
+// Return All events
+router.get('/', nopass, (req, res) => {
+  Events.find()
+    .then(event => {
+      res.json(event);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: 'Error Fetching events!', err });
+    });
 });
 
-// Return and Event by event_id
-router.get('/findById/:events_id', nopass , (req, res, next) => {
-
+// Return an Event by event_id
+router.get(
+  '/findbyid/:event_id',
+  nopass,
+  (req, res, next) => {
     const { event_id } = req.params;
-
     Events.findById(event_id)
-    .where({event_id})
-.then(event => {
-    res.json(event);
-})
-.catch(err => {
- console.log(err);
- res.status(500).json({ message: "Error Fetching Event!"});
-
-})
-
-});
+      .then(event => {
+        res.json(event);
+      })
+      .catch(err => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ message: 'Error Fetching Event!', err });
+      });
+  }
+);
 
 // Return Events by user_id
-router.get('/byuserid/:user_id', nopass , (req, res, next) => {
-
+router.get(
+  '/byuserid/:user_id',
+  nopass,
+  (req, res, next) => {
     const { user_id } = req.params;
 
     Events.findByUserId(user_id)
-    .where({user_id})
-.then(event => {
-    res.json(event);
-})
-.catch(err => {
- console.log(err);
- res.status(500).json({ message: "Error Fetching Events!"});
-
-})
-
-});
+      .then(event => {
+        res.json(event);
+      })
+      .catch(err => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ message: 'Error Fetching Events!', err });
+      });
+  }
+);
 
 // Insert an Event
 router.post('/insertevents', nopass, (req, res) => {
-    
-    const EventData = req.body;
-  
-    Events.add(EventData)
+  const EventData = req.body;
+
+  Events.add(EventData)
     .then(event => {
       res.status(201).json(event);
     })
-    .catch (err => {
-      res.status(500).json({ message: 'Failed to create new event!', err });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: 'Failed to create new event!',
+        err
+      });
     });
-  });
-
+});
 
 // Delete an Event
-router.delete('/deleteevent/:id', nopass, (req, res) => {
-    const { id } = req.params;
-  
-    Events.remove(id)
-    .then(deleted => {
-      if (deleted) {
-        res.json({ removed: deleted });
+router.delete(
+  '/deleteevent/:event_id',
+  nopass,
+  (req, res) => {
+    const { event_id } = req.params;
+
+    Events.remove(event_id)
+      .then(deleted => {
+        if (deleted) {
+          res.status(200).json({ removed: deleted });
+        } else {
+          res.status(400).json({
+            message: 'Could not find event with given id',
+            err
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ message: 'Failed to delete event', err });
+      });
+  }
+);
+
+// update an event
+router.put('/updateevent/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  Events.findById(id)
+    .then(event => {
+      if (event) {
+        Events.update(id, changes).then(updatedEvent => {
+          res.status(200).json(updatedEvent);
+        });
       } else {
-        res.status(404).json({ message: 'Could not find event with given id' });
+        res.status(400).json({
+          message: 'Could not find event with given id!',
+          err
+        });
       }
     })
     .catch(err => {
-      res.status(500).json({ message: 'Failed to delete event' });
+      res
+        .status(500)
+        .json({ message: 'Failed to update event!', err });
     });
-  });
+});
 
-  // update an event 
-  router.put('/updateevent/:id', (req, res) => {
-    const { id } = req.params;
-    const changes = req.body;
-  
-    Events.findById(id)
-    .then(event => {
-      if (event) {
-        Events.update(changes, id)
-        .then(updatedEvent => {
-          res.json(updatedEvent);
-        });
-      } else {
-        res.status(404).json({ message: 'Could not find event with given id!' });
-      }
-    })
-    .catch (err => {
-      res.status(500).json({ message: 'Failed to update event!' });
-    });
-  });
-
-
-module.exports = router; 
+module.exports = router;
