3e1a68a05437e05bd9723d0c629a05d1e1a43ff9
sagdish <forsi1@yahoo.com>
2020-03-11 17:05:37 -0700

    authentification and post event flow working

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma }
+  context: request => {
+    return {
+      ...request,
+      prisma
+    }
+  },
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,21 +1,67 @@
-function createUser(root, args, context, info) {
-  return context.prisma.createUser({
-    username: args.username,
-    password: args.password,
-    email: args.email,
-  });
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
+// this is old implementation:
+// function createUser(root, args, context, info) {
+//   return context.prisma.createUser({
+//     username: args.username,
+//     password: args.password,
+//     email: args.email,
+//   });
+// }
+
+async function signup(parent, args, context, info) {
+  // hash password first
+  const password = await bcrypt.hash(args.password, 10)
+  //insert user to database
+  const user = await context.prisma.createUser({
+    ...args,
+    password
+  })
+  // generate token:
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return {
+    token,
+    user,
+  }
+}
+
+async function login(parent, args, context, info) {
+  // get user from DB
+  const user = await await context.prisma.user({ email: args.email })
+  // if user doesnt exists, throw error
+  if (!user) {
+    throw new Error('No such user fount')
+  }
+
+  // validate password:
+  const valid = await bcrypt.compare(args.password, user.password)
+  if (!valid) {
+    throw new Error('Invalid password')
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return {
+    token,
+    user,
+  }
 }
 
 function createEvent(root, args, context, info) {
+  const userId = getUserId(context)
   return context.prisma.createEvent({
     title: args.title,
     description: args.description,
-    createdBy: { connect: { id: args.userId}}
+    createdBy: { connect: { id: userId } },
   })
 }
 
 
 module.exports = {
-  createUser,
-  createEvent
+  signup,
+  login,
+  createEvent,
 }
