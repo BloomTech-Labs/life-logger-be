96c5c0f4a48849c62c496056a7b9e4926c0d1db1
Carl-Stanley <cds.beml@gmail.com>
2020-04-14 19:02:31 -0700

    Commit

--- migrations/20200310000026_users.js
+++ migrations/20200310000026_users.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
-  return knex.schema
-    .createTable('users', function(table) {
+  return knex.schema  
+   .createTable('users', function(table) {
       table.increments();
       table.string('username').notNullable();
       table.string('password').notNullable();
