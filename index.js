9928621c08c1a95bb712c193553a1eb0c83b480e
kly001 <karenly1961@gmail.com>
2020-04-15 22:09:13 -0400

    new tests branch

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const server = require('./api/server.js');
 
-<<<<<<< HEAD
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => console.log(`\n*** Server running on PORT ${PORT} ***\n`));
-=======
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
   console.log(`\n*** Server running on PORT ${PORT} ***\n`)
 );
->>>>>>> 7195c588e860c56c99e89f47c9a915cd9ed7dc48
