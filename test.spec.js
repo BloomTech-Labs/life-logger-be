a2bd0c79f9d735622319ba5eb86d7c817ee2809e
kly001 <karenly1961@gmail.com>
2020-04-16 22:16:36 -0400

    2nd server test

--- test.spec.js
+++ test.spec.js
@@ -45,15 +45,22 @@ describe('POST', ()=> {
 // Testing server
 describe("server.js", () => {
     describe("GET/", ()=> {
-    it('should return 200 OK', () => {
-        request(server).get("/").expect(200)
+    it('should return 200 OK', async() => {
+        const response = await request(server).get("/");
+        expect(response.status).toBe(200);
     });
 
-    it.todo('should return JSON');
+    it('should return JSON', () => {
+        return request(server).get("/") 
+        .then(res => {
+             expect(res.type).toMatch(/json/i)
+        })
+     });
+ 
 
 
     it.todo('should respond with { "api: Up and running" }');
-})
+    })
 })
 
 
