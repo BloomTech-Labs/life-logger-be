74e0f5724956164f7abfeb338695393a44a77daa
Janessa Garrow <janessa.garrow@gmail.com>
2020-07-14 22:56:49 -0600

    Add more functions to update task_names table

--- tasks/task-names-model.js
+++ tasks/task-names-model.js
@@ -1,11 +1,21 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
+  findBy,
   createTaskName,
+  updateTaskName,
+  deleteTaskName,
 };
 
+// filter is an object with the following options for key names:
+// id
+// name
+// user_id -- although this would be redundant
+function findBy(filter, user_id) {
+  return db('task_names').where(filter).where({ user_id });
+}
+
 // add new task to `task_names` table
-// returns an array of the `id` of the newly created `task_name`
 function createTaskName(task) {
   // lower case taskName before adding it to db
   const lowercaseName = task.name.toLowerCase();
@@ -15,5 +25,15 @@ function createTaskName(task) {
     user_id: task.user_id,
   };
 
-  return db('task_names').insert(newTaskName).returning('id');
+  return db('task_names').insert(newTaskName);
+}
+
+// edit task in `task_names` table by the id and user_id
+function updateTaskName(id, user_id, changes) {
+  return db('task_names').where({ id }).where({ user_id }).update(changes, '*');
+}
+
+// delete task in `task_names` table by the id and user_id
+function deleteTaskName(id, user_id) {
+  return db('task_names').where({ id }).where({ user_id }).del();
 }
