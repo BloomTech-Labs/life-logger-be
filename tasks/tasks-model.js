457f062adaa5108d84f53842eb74ac00c76bf928
Janessa Garrow <janessa.garrow@gmail.com>
2020-07-14 22:56:18 -0600

    Update knex queries to reflect updated tables

--- tasks/tasks-model.js
+++ tasks/tasks-model.js
@@ -15,17 +15,46 @@ function findBy(filter, user_id) {
 
 // Get tasks by user ID
 function findByUserId(user_id) {
-  return db('tasks').where({ user_id });
+  // each task needs to return the id, task_id, task_notes, due_date, category_name (get via category_id), and task_name (get via task_id)
+  return db('tasks')
+    .select(
+      'tasks.id',
+      'tasks.user_id',
+      'categories.name as category_name',
+      'task_names.name as task_name',
+      'tasks.task_notes',
+      'tasks.due_date',
+      'tasks.all_day',
+      'tasks.is_complete'
+    )
+    .from('tasks')
+    .innerJoin('task_names', 'tasks.task_id', '=', 'task_names.id')
+    .innerJoin('categories', 'tasks.category_id', '=', 'categories.id')
+    .where({ ['tasks.user_id']: user_id });
 }
 
-// Get tasks by task ID
+// Get tasks by task ID (not the task_id)
 function findById(id, user_id) {
-  return db('tasks').where({ id }).where({ user_id });
+  return db('tasks')
+    .select(
+      'tasks.id',
+      'tasks.user_id',
+      'categories.name as category_name',
+      'task_names.name as task_name',
+      'tasks.task_notes',
+      'tasks.due_date',
+      'tasks.all_day',
+      'tasks.is_complete'
+    )
+    .innerJoin('task_names', 'tasks.task_id', '=', 'task_names.id')
+    .innerJoin('categories', 'tasks.category_id', '=', 'categories.id')
+    .where({ ['tasks.id']: id })
+    .where({ ['tasks.user_id']: user_id });
 }
 
 // Add a new task, returns the whole task
 function add(task) {
-  return db('tasks').insert(task).returning('id');
+  return db('tasks').insert(task);
 }
 
 // Update existing task, returns the whole task
